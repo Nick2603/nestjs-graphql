a5b21874884d7fb07bb921b905dcd483565890f9
Mykyta Vostrikov <mykytavostrikov@gmail.com>
2025-05-09 08:36:27 +0300

    prisma migrate inside docker possible fix

--- src/infrastructure/app-config/app-config.set-up.ts
+++ src/infrastructure/app-config/app-config.set-up.ts
@@ -3,7 +3,7 @@ import configuration from './configuration';
 import { validationOptions, validationSchema } from './validation';
 
 export const appConfigSetUp: ConfigModuleOptions = {
-  envFilePath: ['.env.development'],
+  envFilePath: ['.env'],
   cache: true,
   expandVariables: true,
   isGlobal: false,
